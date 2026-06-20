c1910874c41d0ceb26105681b0bf040bb9d0b96b
LENOVO <embeddedfreak2020@gmail.com>
2026-06-20 21:12:55 +0530

    Subarray sum better approach(Running sum approach added)

---
+++ DSA_ARRAY/subarray_sum_ba.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main()
+{
+	vector<int> arr = {1, 2, 3, 4, 5};
+	int n = arr.size();
+
+	int maxSum = INT_MIN;
+
+	for(int st = 0; st < n; st++) {
+		int sum = 0;
+		for(int end = st; end < n; end++) {
+			sum += arr[end];		
+			maxSum = (maxSum < sum)? sum : maxSum;
+		}
+	}
+
+	cout<<"Max Sum is:"<<maxSum<<endl;
+
+}
+
