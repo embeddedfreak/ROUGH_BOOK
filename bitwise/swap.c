b70afc5ba4aa7682896b0f88e73bee82a26e0fdc
LENOVO <embeddedfreak2020@gmail.com>
2026-07-12 01:24:16 +0530

    swap using bitwise added

---
+++ bitwise/swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+	*a = *a ^ *b;
+	*b = *a ^ *b;
+	*a = *a ^ *b;
+}
+
+int main() {
+	int a = 10;
+	int b = 20;
+
+	swap(&a, &b);
+
+	printf("After swap numbers are: a = %d, b = %d\n", a, b);
+}
