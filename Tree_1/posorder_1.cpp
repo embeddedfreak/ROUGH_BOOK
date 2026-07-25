3fcb7db5aea792c9adb367ffc018ae082b81b417
LENOVO <embeddedfreak2020@gmail.com>
2026-07-25 12:00:00 +0530

    Inorder traversal

---
+++ Tree_1/posorder_1.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Node {
+public: 
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int val) {
+		data = val;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int> preorder)
+{
+	idx++;
+
+	if(preorder[idx] == -1)
+		return NULL;
+
+	Node* root = new Node(preorder[idx]);
+
+	root->left = buildTree(preorder);
+	root->right = buildTree(preorder);
+
+	return root;
+}
+
+void inorder_traverse(Node* root)
+{
+	if(root == NULL)
+		return;
+
+
+	inorder_traverse(root->left);
+	inorder_traverse(root->right);
+	cout<<root->data<<" ";
+}
+
+int main()
+{
+	vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+	Node* root = buildTree(preorder);
+
+	inorder_traverse(root);	
+	return 0;
+}
