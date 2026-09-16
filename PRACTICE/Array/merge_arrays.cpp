6a1547896271c0bcd448f341e66c6d5d9f45e50e
LENOVO <embeddedfreak2020@gmail.com>
2026-09-16 22:30:17 +0530

    Merge 2 arrays

---
+++ PRACTICE/Array/merge_arrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr1[3] = {1, 2, 3};
+	int arr2[3] = {4, 5, 6};
+
+	int merged[6];
+
+	for(int i = 0; i < 3; i++) {
+		merged[i] = arr1[i];
+	}
+
+	for(int i = 0; i < 3; i++) {
+		merged[i+3] = arr2[i];
+	}
+
+
+	for(int idx = 0; idx < 6; idx++) {
+		cout<<merged[idx]<<" ";
+	}
+
+	cout<<endl;
+	return 0;
+}
